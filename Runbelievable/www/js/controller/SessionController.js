a33310701b8de26a5b9b6c4d3100c0d9ae85c451
Siddi Steven <jokho26@gmail.com>
2014-02-24 13:37:28 +0100

    Début de la géolocalisation
    Ajout du module Geolocation de Cordova + utilisation basique

--- Runbelievable/www/js/controller/SessionController.js
+++ Runbelievable/www/js/controller/SessionController.js
@@ -7,7 +7,31 @@ function SessionController($scope) {
 			{test : 'aaa4', testaaa : 'bbb4'}
 	];
 
-	$scope.test=function() {
+	$scope.acquisition=function() {
 		console.log($scope.liste[0].test);
+
+		// onSuccess Callback
+		// This method accepts a Position object, which contains the
+		// current GPS coordinates
+		//
+		var onSuccess = function(position) {
+		    alert('Latitude: '          + position.coords.latitude          + '\n' +
+		          'Longitude: '         + position.coords.longitude         + '\n' +
+		          'Altitude: '          + position.coords.altitude          + '\n' +
+		          'Accuracy: '          + position.coords.accuracy          + '\n' +
+		          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+		          'Heading: '           + position.coords.heading           + '\n' +
+		          'Speed: '             + position.coords.speed             + '\n' +
+		          'Timestamp: '         + position.timestamp                + '\n');
+		};
+
+		// onError Callback receives a PositionError object
+		//
+		function onError(error) {
+		    alert('code: '    + error.code    + '\n' +
+		          'message: ' + error.message + '\n');
+		}
+
+		navigator.geolocation.getCurrentPosition(onSuccess, onError);
 	}
 }
