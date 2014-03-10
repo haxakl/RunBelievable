f425c5fc97966557df252cf8e24da1b7e4819f97
Siddi Steven <jokho26@gmail.com>
2014-03-10 15:33:45 +0100

    Début du graphique
    Ajout des libs de RaphaelJS et début d'un graphique
    #20

--- Runbelievable/www/js/controller/StatistiquesSessionController.js
+++ Runbelievable/www/js/controller/StatistiquesSessionController.js
@@ -126,7 +126,33 @@ function StatistiquesSessionController($scope) {
 			// On ajoute les informations
 			$scope.tableauStats.push(stats);
 			
-		}		
+		}
+		
+		dessinerGraph();
+	}
+	
+	function dessinerGraph() {
+		var r = Raphael("simpleExample");
+		var chart = r.linechart(
+		    10, 10,      // top left anchor
+		    490, 180,    // bottom right anchor
+		    [
+		      [1, 2, 3, 4, 5, 6, 7],        // red line x-values
+		      [3.5, 4.5, 5.5, 6.5, 7, 8]    // blue line x-values
+		    ],
+		    [
+		      [12, 32, 23, 15, 17, 27, 22], // red line y-values
+		      [10, 20, 30, 25, 15, 28]      // blue line y-values
+		    ],
+		    {
+		       nostroke: false,   // lines between points are drawn
+		       axis: "0 0 1 1",   // draw axes on the left and bottom
+		       smooth: true,      // curve the lines to smooth turns on the chart
+		       colors: [
+		         "#995555",       // the first line is red 
+		         "#555599"        // the second line is blue
+		       ]
+		     });
 	}
 	
 	
