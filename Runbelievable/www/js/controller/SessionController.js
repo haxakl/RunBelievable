987d8bf4c5860e9ca948a01d6f2240ef8f382c2f
Coda stephen <coda.stephen@gmail.com>
2014-04-07 13:06:31 +0200

    Chrono

--- Runbelievable/www/js/controller/SessionController.js
+++ Runbelievable/www/js/controller/SessionController.js
@@ -84,6 +84,10 @@ function SessionController($scope, Global) {
         if (!$scope.gestionnaires.gps.actif || $scope.gestionnaires.gps.gps_acquisition_actif) {
             return false;
         }
+        
+        // Lancement du chrono
+        chronoContinue();
+        
         $scope.texte_bouton_acquisition = dico_bouton_acquisition.STOP;
 
         // Acquisition démarée
@@ -125,7 +129,7 @@ function SessionController($scope, Global) {
         if (!$scope.gestionnaires.gps.actif || !$scope.gestionnaires.gps.gps_acquisition_actif) {
             return false;
         }
-
+        chronoStop();
         $scope.texte_bouton_acquisition = dico_bouton_acquisition.RESTART;
 
         // Acquisition arêtée
@@ -214,4 +218,52 @@ function SessionController($scope, Global) {
     // Test si le Gps est allumé    
     $scope.gestionnaires.gps.isEnabled($scope.initializeMap);
 
+}
+
+// Chronometre
+
+var startTime = 0
+var start = 0
+var end = 0
+var diff = 0
+var timerID = 0
+function chrono(){
+	end = new Date()
+	diff = end - start
+	diff = new Date(diff)
+	var msec = diff.getMilliseconds()
+	var sec = diff.getSeconds()
+	var min = diff.getMinutes()
+	var hr = diff.getHours()-1
+	if (min < 10){
+		min = "0" + min
+	}
+	if (sec < 10){
+		sec = "0" + sec
+	}
+	if(msec < 10){
+		msec = "00" +msec
+	}
+	document.getElementById("chronotime").innerHTML = hr + ":" + min + ":" + sec 
+	timerID = setTimeout("chrono()", 10)
+}
+function chronoStart(){
+	start = new Date()
+	chrono()
+}
+function chronoContinue(){
+	start = new Date()-diff
+	start = new Date(start)
+	chrono()
+}
+function chronoReset(){
+	document.getElementById("chronotime").innerHTML = "0:00:00"
+	start = new Date()
+}
+function chronoStopReset(){
+	document.getElementById("chronotime").innerHTML = "0:00:00"
+	document.chronoForm.startstop.onclick = chronoStart
+}
+function chronoStop(){
+	clearTimeout(timerID)
 }
